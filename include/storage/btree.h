7cc8a7ce025ef684b9b8464a3d4419f64f4fee19
Jingze Xing <xingjz20@outlook.com>
2023-11-19 23:57:08 +0800

    stash

--- include/storage/btree.h
+++ include/storage/btree.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include <cstdint>
+#include <memory>
+#include <optional>
 #include <vector>
 
 #include <engine/defs.h>
 #include <storage/defs.h>
+#include <utils/config.h>
 
 enum NodeType : uint8_t {
   INTERNAL = 0,
@@ -14,14 +17,26 @@ enum NodeType : uint8_t {
 struct BPlusQueryResult {
   int pagenum;
   int slotnum;
+  uint8_t *ptr;
 };
 
 struct BPlusNodeMeta {
+  int parent; // pagenum
+  /// for first child, its change in key will affect its parent's key.
+  /// when it goes below minimum capacity, it borrows from its right sibling
   int left_sibling;  // pagenum
   int right_sibling; // pagenum
   int size;
   int next_empty;
   NodeType type;
+  bool is_first_child;
+  void reset() {
+    parent = left_sibling = right_sibling = -1;
+    size = 0;
+    next_empty = -1;
+    type = NodeType::INTERNAL;
+    is_first_child = false;
+  }
 };
 
 /// keys are stored seperate from data
@@ -30,23 +45,65 @@ struct BPlusNodeMeta {
 /// for leaf nodes, data equals to
 /// - record location(page, slot), serves as a unique identifier for a record
 /// - corresponding record
-/// an internal node matches like [ key[i], key[i+1] )
+/// an internal node matches like [ key[i], key[i+1] ), data num = key num + 1
+/// a leaf node performs exact match, data num = key num
 class BPlusTree {
 private:
   int fd, pagenum_root;
   int key_num;      /// a composite key consists of key_num INTs
-  int internal_cap; /// key size + pagenum
-  int leaf_cap;     /// key size + record size + record(pagenum, slotnum)
+  int internal_max; /// key size + pagenum
+  int leaf_max;     /// key size + record size + record(pagenum, slotnum)
+  int record_len;
+  int leaf_data_len;
 
-  /// find largest index i such that a[i] </<= key
-  /// find smallest index i such that a[i] >/>= key
-  /// return i if a[i] == key && op == EQ
-  /// use <= when querying internal nodes
   int compare_key(const int *a, const int *b) const;
-
+  /// find largest index i such that a[i] </<= key.
+  /// find smallest index i such that a[i] >/>= key.
+  /// return i if a[i] == key && op == EQ.
+  /// use <= when querying internal nodes.
   int bin_search(int *a, int len, const std::vector<int> &key,
                  Operator op) const;
 
 public:
-  int precise_match(const std::vector<int> &key) const;
+  BPlusTree(int fd, int pagenum_root, int key_num, int record_len)
+      : fd(fd), pagenum_root(pagenum_root), key_num(key_num),
+        record_len(record_len) {
+    leaf_data_len = record_len + sizeof(int) * 2;
+    /// (key): int
+    internal_max = (Config::PAGE_SIZE - sizeof(BPlusNodeMeta) - sizeof(int)) /
+                   (sizeof(int) * (key_num + 1));
+    /// (key): leaf_data
+    leaf_max = (Config::PAGE_SIZE - sizeof(BPlusNodeMeta)) /
+               (sizeof(int) * key_num + leaf_data_len);
+  }
+  BPlusTree(int fd, SequentialAccessor &accessor);
+
+  inline int get_cap(NodeType type) const {
+    return type == INTERNAL ? internal_max : leaf_max;
+  }
+
+  std::optional<BPlusQueryResult>
+  precise_match(const std::vector<int> &key) const;
+
+  void insert(const std::vector<int> &key, int rec_page, int rec_slot,
+              const uint8_t *record);
+
+  void serialize();
+};
+
+/// manage multiple BPlusTree in a single file
+class BPlusForest {
+private:
+  int fd, n_pages, ptr_available;
+  std::vector<std::shared_ptr<BPlusTree>> trees;
+
+public:
+  /// build from scratch
+  BPlusForest(int fd) : fd(fd){};
+  /// build from file
+  BPlusForest(int fd, SequentialAccessor &accessor);
+  ~BPlusForest() { trees.clear(); }
+  void serialize();
+  int alloc_page();
+  int free_page();
 };
--- src/storage/btree.cpp
+++ src/storage/btree.cpp
@@ -1,3 +1,6 @@
+#include "utils/config.h"
+#include <cstring>
+
 #include <storage/btree.h>
 #include <storage/paged_buffer.h>
 
@@ -24,12 +27,12 @@ int BPlusTree::bin_search(int *a, int len, const std::vector<int> &key,
   while (l <= r) {
     mid = (l + r) >> 1;
     int result = compare_key(a + mid * key_num, key.data());
-    if (a[mid] < key[0]) {
+    if (result == -1) { /// a[mid] < key[0]
       l = mid + 1;
       if (op == Operator::LT || op == Operator::LE) {
         ans = mid;
       }
-    } else if (a[mid] > key[0]) {
+    } else if (result == 1) { /// a[mid] > key[0]
       r = mid - 1;
       if (op == Operator::GT || op == Operator::GE) {
         ans = mid;
@@ -60,29 +63,66 @@ int BPlusTree::bin_search(int *a, int len, const std::vector<int> &key,
   return ans;
 }
 
-int BPlusTree::precise_match(const std::vector<int> &key) const {
+std::optional<BPlusQueryResult>
+BPlusTree::precise_match(const std::vector<int> &key) const {
   int pagenum_cur = pagenum_root;
   while (true) {
     uint8_t *slice =
         PagedBuffer::get()->read_file(std::make_pair(fd, pagenum_cur));
     BPlusNodeMeta *meta = (BPlusNodeMeta *)slice;
     int *keys = (int *)(slice + sizeof(BPlusNodeMeta));
-    int *data = keys + key_num * meta->size;
+    int *data = keys + key_num * get_cap(meta->type);
     if (meta->type == NodeType::LEAF) {
       int idx = bin_search(keys, meta->size, key, Operator::EQ);
       if (idx == -1 || compare_key(keys + idx * key_num, key.data()) != 0) {
-        return -1;
+        return std::nullopt;
       }
-      return data[idx];
+      return (BPlusQueryResult){pagenum_cur, idx,
+                                ((uint8_t *)data) + leaf_data_len * idx};
     } else {
-      int idx = bin_search(keys, meta->size, key, Operator::LE);
-      if (idx == -1) {
-        pagenum_cur = meta->left_sibling;
-      } else if (idx == meta->size) {
-        pagenum_cur = meta->right_sibling;
-      } else {
-        pagenum_cur = data[idx];
-      }
+      int idx = bin_search(keys, meta->size, key, Operator::LE) + 1;
+      pagenum_cur = data[idx];
     }
   }
+}
+
+uint8_t bbuf[Config::PAGE_SIZE];
+
+void BPlusTree::insert(const std::vector<int> &key, int rec_page, int rec_slot,
+                       const uint8_t *record) {
+  int pagenum_cur = pagenum_root;
+  uint8_t *slice, *data;
+  BPlusNodeMeta *meta;
+  int *keys;
+  while (true) {
+    slice = PagedBuffer::get()->read_file(std::make_pair(fd, pagenum_cur));
+    keys = (int *)(slice + sizeof(BPlusNodeMeta));
+    meta = (BPlusNodeMeta *)slice;
+    data = (uint8_t *)(keys + key_num * get_cap(meta->type));
+    if (meta->type == NodeType::LEAF) {
+      break;
+    } else {
+      int idx = bin_search(keys, meta->size, key, Operator::LE) + 1;
+      pagenum_cur = ((int *)data)[idx];
+    }
+  }
+  if (meta->size < leaf_max) {
+    int sz = meta->size;
+    ++meta->size;
+    int pos = bin_search(keys, meta->size, key, Operator::LE) + 1;
+    memcpy(bbuf, keys + pos * key_num, (sz - pos) * key_num * sizeof(int));
+    memcpy(keys + (pos + 1) * key_num, bbuf,
+           (sz - pos) * key_num * sizeof(int));
+    memcpy(keys + pos * key_num, key.data(), key_num * sizeof(int));
+    memcpy(bbuf, data + pos * leaf_data_len, (sz - pos) * leaf_data_len);
+    memcpy(data + (pos + 1) * leaf_data_len, bbuf, (sz - pos) * leaf_data_len);
+    ((int *)(data + pos * leaf_data_len))[0] = rec_page;
+    ((int *)(data + pos * leaf_data_len))[1] = rec_slot;
+    memcpy(data + pos * leaf_data_len + 2 * sizeof(int), record, leaf_data_len);
+    return;
+  }
+  /// (key_buf: val) is the new seperator to the parent
+  static std::vector<int> key_pushup;
+  int val_pushup = -1;
+  key_pushup.resize(key_num);
 }
